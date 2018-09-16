a3ced539bda37ec67f96fabf0a9de4fc79c53e09
Baruch Oltman <baruch.oltman@outlook.com>
2018-09-16 12:01:44 -0500

    Client only connected locally before, working on allowing WAN connection

--- code/ImClient.cpp
+++ code/ImClient.cpp
@@ -12,9 +12,10 @@ Baruch Oltman September 2018
 // TODO(baruch): Only supporting ascii consider Unicode later
 #undef UNICODE
 
-#define DEFAULT_PORT "5555"
+#define DEFAULT_PORT 5555
 #define DEFAULT_BUFLEN 512
-#define SERVER_IPV4 "10.50.212.152"
+#define SERVER_IPV4 "199.184.143.92"
+
 
 int main(int argc, char **argv)
 {
@@ -44,38 +45,15 @@ int main(int argc, char **argv)
     
     
     /**********************Socket Code Here**********************/
+    //convert ip string to binary format for use in SOCKADDR_IN struct.
+    int iP4;
+    inet_pton(AF_INET, SERVER_IPV4, &iP4);
     
+    SOCKADDR_IN SockAddrIP4;
+    SockAddrIP4.sin_family = AF_INET;
+    SockAddrIP4.sin_addr.s_addr = iP4;
+    SockAddrIP4.sin_port = htons(DEFAULT_PORT);
     
-    /**********************Get address info*************/
-    struct addrinfo *result = NULL;
-    //*ptr is used to access each addrinfo struct returned in result as a linked list.
-    struct addrinfo *ptr = NULL;
-    //hints is a struct to hold basic information about which type of socket
-    //the caller (in this case server) supports. Must set zero out unused members
-    struct addrinfo hints;
-    
-    //Zero unused hints members to prevent error
-    hints.ai_addrlen = 0;
-    hints.ai_canonname = NULL;
-    hints.ai_addr = NULL;
-    hints.ai_next = NULL;
-    
-    // TODO(baruch): Currently only supporting IPv4, consider IPv6 later
-    hints.ai_family = AF_INET;
-    // This is a stream socket i.e. tcp/ip
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
-    
-    //gives list of addresses based on port and type?
-    char serverIPv4[] = SERVER_IPV4;
-    int resultCode = getaddrinfo(serverIPv4, DEFAULT_PORT, &hints, &result);
-    
-    if(resultCode != 0)
-    {
-        printf("getaddrinfo failed with error: %d\n", resultCode);
-        WSACleanup();
-        return 1;
-    }
     /**************Create Socket****************/
     
     //INVALID_SOCKET used like NULL
@@ -83,21 +61,19 @@ int main(int argc, char **argv)
     
     // TODO(baruch): Only supporting IP_V4
     // TODO(baruch): ptr checks first struct of linked list only, this may need to be changed for release.
-    ptr = result;
-    ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+    ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     
     if(ConnectSocket == INVALID_SOCKET)
     {
         printf("socket() error: %ld\n", WSAGetLastError());
         //clean up address info after getaddrinfo function when socket fails
-        freeaddrinfo(result);
         WSACleanup();
         return 1;
     }
     
     /*****************Connect to socket**************/
     
-    int connectResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+    int connectResult = connect(ConnectSocket, (SOCKADDR*)&SockAddrIP4, sizeof(SOCKADDR_IN));
     if(connectResult == SOCKET_ERROR)
     {
         printf("Connect failed with error: %d\n", WSAGetLastError());
@@ -106,12 +82,11 @@ int main(int argc, char **argv)
     }
     else
     {
-        printf("Connected with server: %s\n", serverIPv4);
+        printf("Connected with server: %s\n", SERVER_IPV4);
     }
     
     // TODO(baruch):If connection fails move ptr to next address struct in linked list and try again. For testing, just trying first address.
     
-    freeaddrinfo(result);
     
     if(ConnectSocket == INVALID_SOCKET)
     {
